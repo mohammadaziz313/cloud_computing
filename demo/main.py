9e52f648d15b0fcd3570c5a6bd8a867ccf175541
Farshid Rayhan <32468549+farshidrayhanuiu@users.noreply.github.com>
2018-06-23 23:47:25 +0600

    Add files via upload

---
+++ demo/main.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, flash, request, jsonify
+from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
+import requests
+import json
+import logging
+import requests_toolbelt.adapters.appengine
+
+requests_toolbelt.adapters.appengine.monkeypatch()
+
+try:
+    # For Python 3.0 and later
+    from urllib.error import HTTPError
+    from urllib.parse import quote
+    from urllib.parse import urlencode
+except ImportError:
+    # Fall back to Python 2's urllib2 and urllib
+    from urllib2 import HTTPError
+    from urllib import quote
+    from urllib import urlencode
+
+# App config.
+DEBUG = True
+app = Flask(__name__)
+
+
+
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    return render_template('index.html')
+
+@app.route("/map", methods=['GET', 'POST'])
+def map():
+    return render_template('map.html')
+
+
+if __name__ == '__main__':
+    app.run()
+
